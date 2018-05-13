ccd60b7a7170627a60355b8319bced29d3d5deaf
szalontaycsaba <szalontaycsaba@users.noreply.github.com>
2018-05-13 21:20:46 +0200

    javit x+3

--- jatekmester.cpp
+++ jatekmester.cpp
@@ -9,8 +9,8 @@ using namespace genv;
 
 bool Jatekmester::ellenorzo(std::vector<int> myvec,std::vector<int> myvec2)
 {
-    sort(myvec.begin(),myvec.end());
-    sort(myvec2.begin(),myvec2.end());
+//    sort(myvec.begin(),myvec.end());
+//    sort(myvec2.begin(),myvec2.end());
 
     if(myvec.size() > 5)
     {
@@ -21,7 +21,74 @@ bool Jatekmester::ellenorzo(std::vector<int> myvec,std::vector<int> myvec2)
             pw = 1;
             for(unsigned j = 1 ; j < myvec.size()-1 ; j++)
             {
-                if(myvec[i] == (myvec[i+j])-j && myvec2[i] == (myvec2[i+j])-j)
+                if(myvec[i] > myvec[i+j])
+                {
+                    if(myvec[i] == (myvec[i+j])+j && myvec2[i] == (myvec2[i+j])-j)
+                    {
+                        pw++;
+                        if(pw == 5)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                if(myvec2[i] > myvec2[i+j])
+                {
+                    if(myvec[i] == (myvec[i+j])-j && myvec2[i] == (myvec2[i+j])+j)
+                    {
+                        pw++;
+                        if(pw == 5)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                if(myvec[i] > myvec[i+j] && myvec2[i] > myvec2[i+j])
+                {
+                    if(myvec[i] == (myvec[i+j])+j && myvec2[i] == (myvec2[i+j])+j)
+                    {
+                        pw++;
+                        if(pw == 5)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                if (myvec[i] == (myvec[i+j])-j && myvec2[i] == (myvec2[i+j])-j)
+                {
+                    pw++;
+                    if(pw == 5)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+
+        pw=1;
+        for(unsigned i = 1 ; i < myvec.size()-1 ; i++)
+        {
+            pw = 1;
+
+            for(unsigned j = 1 ; j < myvec.size()-1 ; j++)
+            {
+                if(myvec[i] > myvec[i+j])
+                {
+                    if(myvec[i] == (myvec[i+j])+j && myvec2[i] == myvec2[i+j])
+                    {
+                        pw++;
+                        if(pw == 5)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                if (myvec[i] == (myvec[i+j])-j && myvec2[i] == myvec2[i+j])
                 {
                     pw++;
                     if(pw == 5)
@@ -35,24 +102,20 @@ bool Jatekmester::ellenorzo(std::vector<int> myvec,std::vector<int> myvec2)
         for(unsigned i = 1 ; i < myvec.size()-1 ; i++)
         {
             pw = 1;
-            for(unsigned j = 1 ; j < myvec.size()-1 ; j++)
+            for(unsigned j = 1 ; j < myvec.size() ; j++)
             {
-                if(myvec[i] == (myvec[i+j])-j && myvec2[i] == myvec2[i+j])
+                if(myvec2[i] > myvec2[i+j])
                 {
-                    pw++;
-                    if(pw == 5)
+                    if(myvec2[i] == (myvec2[i+j])+j && myvec[i] == myvec[i+j])
                     {
-                        return true;
+                        pw++;
+                        if(pw == 5)
+                        {
+                            return true;
+                        }
                     }
                 }
-            }
-        }
-        pw=1;
-        for(unsigned i = 1 ; i < myvec.size()-1 ; i++)
-        {
-            pw = 1;
-            for(unsigned j = 1 ; j < myvec.size()-1 ; j++)
-            {
+
                 if(myvec2[i] == (myvec2[i+j])-j && myvec[i] == myvec[i+j])
                 {
                     pw++;
